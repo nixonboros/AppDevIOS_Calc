5cf2a78c61248c75856f7730412a319be94a53f0
nixon <126039614+nixonboros@users.noreply.github.com>
2025-03-11 17:12:23 +1100

    multiple args implemented
    addition and subtraction working as intended

--- calc/main.swift
+++ calc/main.swift
@@ -17,29 +17,46 @@ if args.count < 3 {
 }
 
 // Retrieve User Input
-let no1 = args[0]; // Sample Code Only! Update Required!
-let operatorSymbol = args[1]; // Sample Code Only! Update Required!
-let no2 = args[2]; // Sample Code Only! Update Required!
+//let no1 = args[0]; // Sample Code Only! Update Required!
+//let operatorSymbol = args[1]; // Sample Code Only! Update Required!
+//let no2 = args[2]; // Sample Code Only! Update Required!
 
 // Initialize a Calculator object
 let calculator = Calculator();
 
-// Parse the first number, operator, and second number
-if let num1 = Int(no1), let num2 = Int(no2) {
-    var result = 0
+// Perform calculations (starts with first num)
+if let initialNum = Int(args[0]) {
+    var result = initialNum
     
-    // Perform calc based on the operator
-    switch operatorSymbol {
-    case "+":
-        result = calculator.add(no1: num1, no2: num2)
-    case "-":
-        result = calculator.subtract(no1: num1, no2: num2)
-    default:
-        print("Error: Unsupported operator")
-        exit(1)
+    // Calculations with remaining args (operators and nums)
+    var currentIndex = 1
+    while currentIndex < args.count {
+        let operatorSymbol = args[currentIndex]
+        currentIndex = currentIndex + 1
+        
+        if currentIndex < args.count, let nextNumber = Int(args[currentIndex]) {
+            // Performs calculation based on operator
+            switch operatorSymbol {
+            case "+":
+                result = calculator.add(no1: result, no2: nextNumber)
+            case "-":
+                result = calculator.subtract(no1: result, no2: nextNumber)
+            default:
+                print("Error: Unsupported operator")
+                exit(1)
+            }
+        }
+        else {
+            print("Error: Invalid number after operator")
+            exit(1)
+        }
+        
+        currentIndex = currentIndex + 1
     }
     
     // Output result
-    print(no1 + " " + operatorSymbol + " " + no2) // testing
     print(result)
+}
+else {
+    print("Error: Invalid input")
 }
